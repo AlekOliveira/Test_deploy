4ed5320a2b4159a5f93f8320e11d97efb9c7b5b1
Henrique Ramos <ricci.henrique@outlook.com>
2021-02-19 14:55:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ app.use('/', (req, res) => {
     res.send('<h1>Teste hehe</h1>');
 });
 
-app.listen(80, _ => {
-    console.log('Rodando na porta 80');
+app.listen(7788, _ => {
+    console.log('Rodando na porta 7788');
 })
