b5efed68e2a32138e943e592b10d442bddd5229b
Henrique Ricci <ricci.henrique@outlook.com>
2021-06-23 00:55:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 
 app.use('/', (req, res) => {
-    res.send('<h1>Alexandre Oliveira!</h1>');
+    res.send('<h1>PUTA QUE PARIU</h1>');
 });
 
 app.listen(8087, _ => {
